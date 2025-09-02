2199462f2b72c0acd52af83294e86ce0edf89fe4
EdiosGar <ediosgar@hotmail.com>
2025-09-01 23:28:06 -0600

    Solution

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <?php
-    $request = $_REQUEST['REQUEST_URI'];
-    $viewDir = '/views/';
+// Shear the solution
+$request = substr($_SERVER['REQUEST_URI'], 18);
+// $request = $_SERVER['SERVER_NAME'];
+// $request = str_replace($_SERVER['REQUEST_URI'],'/',$_SERVER['REQUEST_URI']);
+$viewDir = '/views/';
 
-    switch ($request){
-        case '':
-        case '/':
-            require __DIR__ . $viewDir . 'home.php';
-            break;
-        default:
-            http_response_code(404);
-            require __DIR__ . $viewDir . '404.php';
-    }
+switch ($request) {
+    case '':
+    case '/':
+        require __DIR__ . $viewDir . 'home.php';
+        break;
+    default:
+        http_response_code(404);
+        require __DIR__ . $viewDir . '404.php';
+}
+
+echo "Request: $request";
+echo "<br>";
+echo "Ruta home: " . __DIR__ . $viewDir . 'home.php';
 ?>
