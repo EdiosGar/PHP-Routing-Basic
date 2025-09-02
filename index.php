0bd591dc4530c3884d016457acb096f896070306
EdiosGar <ediosgar@hotmail.com>
2025-09-01 22:56:57 -0600

    First routing and add view

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    $request = $_REQUEST['REQUEST_URI'];
+    $viewDir = '/views/';
+
+    switch ($request){
+        case '':
+        case '/':
+            require __DIR__ . $viewDir . 'home.php';
+            break;
+        default:
+            http_response_code(404);
+            require __DIR__ . $viewDir . '404.php';
+    }
+?>
